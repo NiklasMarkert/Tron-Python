c5171ee145df9771ea32d7d0a1559f09b9f29899
Niklas Markert <bt709885@uni-bayreuth.de>
2019-11-26 13:55:20 +0000

    Replace Menu.py

--- Menu.py
+++ Menu.py
@@ -1,21 +1,39 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtGui import QPixmap
-import sys 
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+import game
 
-class Menu:
+
+class Menu(QMainWindow):
+    def start_game1(self):
+        self.hide()
+        g = game.Game1(self)
+        g.show()
+
+    def start_game2(self):
+        print('Snake')
+
+    def add_button(self, pos, text, fkt):
+        button = QPushButton(text, self)
+        button.move(200, pos)
+        button.resize(500, 50)
+        button.setStyleSheet('QPushButton {font: bold; font-size: 30px}')
+        button.clicked.connect(fkt)
+
+    def add_text(self):
+        text = QLabel('TRON', self)
+        text.resize(500, 300)
+        font = text.font()
+        font.setBold(True)
+        font.setPointSize(90)
+        text.setFont(font)
+        text.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+        text.move(200, 0)
+    
     def __init__(self):
-        app = QApplication(sys.argv)
-        win = QMainWindow()
-        #win.top = 1000
-        win.resize(1200,900)
-        win.setWindowTitle("Menu")
-        
-        win.label = QLabel(win)
-        win.label.setPixmap(QPixmap('tron.png'))
-        win.label.setGeometry(0,0, 1200, 900)
-
-        win.show()
-        sys.exit(app.exec_())
-
-m = Menu()
+        super(Menu, self).__init__()
+        self.setWindowTitle('Tron')
+        self.resize(900, 600)
+        self.add_button(280, 'Local Multiplayer', self.start_game1)
+        self.add_button(340, 'Snake Mode', self.start_game2)
+        self.add_text()
