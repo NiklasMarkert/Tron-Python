c222a6de24592e0182cd559d4ebd8e60ed397b26
Niklas Markert <bt709885@uni-bayreuth.de>
2019-12-10 18:37:49 +0000

    Upload New File

---
+++ constants.py
@@ -0,0 +1,13 @@
+#Board
+BOARD_WIDTH = 90    #Breite des Boards (Felder)
+BOARD_HEIGHT = 60   #Höhe des Boards (Felder)
+FIELD_SIZE = 10     #Groesse der Felder auf dem Board (Pixel)
+
+#Farben
+BLUE = [0x00A7B5, 0x00DEF0]
+PINK = [0xD500C1, 0xFE54EE]
+GREEN = 0x53F302
+
+#Fenster
+WINDOW_WIDTH = BOARD_WIDTH * FIELD_SIZE
+WINDOW_HEIGHT = BOARD_HEIGHT * FIELD_SIZE
