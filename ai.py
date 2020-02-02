d110434c410b87c10611692cdef270b1477b330a
Niklas Markert <bt709885@uni-bayreuth.de>
2020-02-02 16:52:59 +0000

    Replace ai.py

--- ai.py
+++ ai.py
@@ -6,6 +6,8 @@ class AI(car.Car):
     def __init__(self, x, y, dir, color, self_dest, player_name):
         super().__init__(x, y, dir, color, self_dest, player_name)
         self.ai = True
+        self.lastTurns = [0, 0]
+        self.turns = 0
 
     def turn_right(self):
         self.dir = (self.dir + 1) % 4
@@ -13,33 +15,39 @@ class AI(car.Car):
     def turn_left(self):
         self.dir = (self.dir + 3) % 4
 
-    def border_detection(self):
-        while(True):
-            if (self.dir == c.NORTH and self.y < 1) \
-            or (self.dir == c.EAST and self.x > c.BOARD_WIDTH - 2) \
-            or (self.dir == c.SOUTH and self.y > c.BOARD_HEIGHT - 2) \
-            or (self.dir == c.WEST and self.x < 1):
-                if random() > 0.5:
-                    self.turn_right()
-                else:
-                    self.turn_left()
-            else:
-                break
-
     def change_direction(self, map):
         r = random()
-        if map[self.dir] < 0 or r < 0.05:
+        if map[self.dir] < 0 or r < 0.1:
+            if self.lastTurns[0] == self.lastTurns[1]:
+                if self.lastTurns[0] == 1 and map[(self.dir + 3) % 4] > 0:
+                    self.turn_left()
+                    self.lastTurns[self.turns] = 2
+                    self.turns = (self.turns+1)%2
+                    return
+                elif map[(self.dir + 1) % 4] > 0:
+                    self.turn_right()
+                    self.lastTurns[self.turns] = 1
+                    self.turns = (self.turns+1)%2
+                    return
             r = random()
             if r < 0.5:
                 if map[(self.dir + 1) % 4] > 0:
                     self.turn_right()
+                    self.lastTurns[self.turns] = 1
+                    self.turns = (self.turns+1)%2
                 elif map[(self.dir + 3) % 4] > 0:
                     self.turn_left()
+                    self.lastTurns[self.turns] = 2
+                    self.turns = (self.turns+1)%2
             else:
                 if map[(self.dir + 3) % 4] > 0:
                     self.turn_left()
+                    self.lastTurns[self.turns] = 2
+                    self.turns = (self.turns+1)%2
                 elif map[(self.dir + 1) % 4] > 0:
                     self.turn_right()
+                    self.lastTurns[self.turns] = 1
+                    self.turns = (self.turns+1)%2
 
 
 
