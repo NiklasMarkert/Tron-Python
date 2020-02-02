192a88260e850d8463ed424df4a5bed0e7d02dbb
Niklas Markert <bt709885@uni-bayreuth.de>
2020-02-02 16:57:14 +0000

    Replace menu.py

--- menu.py
+++ menu.py
@@ -1,37 +1,53 @@
-from PyQt5.QtWidgets import QMainWindow, QLabel, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QLabel, QPushButton, QLineEdit
 from PyQt5.QtCore import Qt, QSize
 from PyQt5.QtGui import QIcon
 import constants as c
 import game
 import options
+import highscore
+import highscore_window
 
 
 class Menu(QMainWindow):
+
+    def change_name(self, name):
+        new_name = ''
+        for i in name:
+            if i != '%':
+                new_name = new_name + i
+        if new_name == '':
+            c.PLAYER_NAME = 'Unknown'
+        else:
+            c.PLAYER_NAME = new_name
     
     def start_loc_multi(self):
         """ Startet des lokalen Mehrspieler Modus
         """
         self.hide()
-        g = game.Game(self, 'lok_multi')
+        g = game.Game(self, 'lok_multi', self.highscore)
+        self.change_name(self.textbox.text())
         g.show()
 
     def start_ai(self):
         """ Startet den Computergegner Modus
         """
         self.hide()
-        g = game.Game(self, 'ai_mode')
+        g = game.Game(self, 'ai_mode', self.highscore)
+        self.change_name(self.textbox.text())
         g.show()
 
     def start_snake(self):
         """ Startet den Snake Modus
         """
         self.hide()
-        g = game.Game(self, 'snake_mode')
+        g = game.Game(self, 'snake_mode', self.highscore)
+        self.change_name(self.textbox.text())
         g.show()
 
-    def start_bonus(self):
-        print('Bonus Mode')
-
+    def start_powerup(self):
+        """ Startet den PowerUp Modus
+        """
+        print('PowerUp Mode')
 
     def start_options(self):
         """ Öffnet die Einstellungen
@@ -39,6 +55,14 @@ class Menu(QMainWindow):
         self.hide()
         o = options.Options(self)
         o.show()
+
+    def show_highscores(self):
+        """ Öffnet die Highscoreanzeige
+        """
+        self.hide()
+        hsw = highscore_window.HighscoreWindow(self, self.highscore)
+        hsw.show()
+        self.highscore.print_out()
 
     def add_button(self, pos, text, fkt):
         """ Fügt einen Butten hinzu um einen Spielmodus zu starten
@@ -73,15 +97,33 @@ class Menu(QMainWindow):
         text.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
         text.move(0, 0)
     
+    def add_textbox(self):
+        """ Fügt eine Textbox hinzu, um den Spielernamen einzugeben
+        """
+        text = QLabel('Player Name:', self)
+        text.resize(150, 20)
+        font = text.font()
+        font.setBold(True)
+        font.setPointSize(10)
+        text.setFont(font)
+        text.move(c.MENU_WINDOW_WIDTH - 160, 10)
+        self.textbox = QLineEdit(self)
+        self.textbox.move(c.MENU_WINDOW_WIDTH - 160, 30)
+        self.textbox.resize(150, 25)
+        self.textbox.setText('Player1')
+
     def __init__(self):
-        """ Konstruktor für das Menue
+        """ Konstruktor für das Menü
         """
         super(Menu, self).__init__()
         self.setWindowTitle('Tron')
-        self.resize(c.MENU_WINDOW_WIDTH, c.MENU_WINDOW_HEIGHT)
+        self.setFixedSize(c.MENU_WINDOW_WIDTH, c.MENU_WINDOW_HEIGHT)
         self.add_text()
-        self.add_button(280, 'Local Multiplayer', self.start_loc_multi)
-        self.add_button(340, 'V.S. AI', self.start_ai)
-        self.add_button(400, 'Snake Mode', self.start_snake)
-        self.add_button(460, 'Bonus Mode', self.start_bonus)
+        self.add_button(260, 'Local Multiplayer', self.start_loc_multi)
+        self.add_button(320, 'V.S. AI', self.start_ai)
+        self.add_button(380, 'Snake Mode', self.start_snake)
+        self.add_button(440, 'PowerUp Mode', self.start_powerup)
+        self.add_button(500, 'Highscores', self.show_highscores)
+        self.add_textbox()
         self.add_options()
+        self.highscore = highscore.Highscore()
