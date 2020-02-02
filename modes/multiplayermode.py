3bc479a2e3c0c49375ad9d184eb6411ee676b102
Niklas Markert <bt709885@uni-bayreuth.de>
2020-02-02 16:59:41 +0000

    Replace multiplayermode.py

--- modes/multiplayermode.py
+++ modes/multiplayermode.py
@@ -1,5 +1,6 @@
 from PyQt5.QtGui import QPainter, QColor
 import constants as c
+from constants import Directions as d
 import expections
 import board
 
@@ -10,8 +11,8 @@ class LokMulti(board.Board):
 
     def start(self):
         self.car_list = []
-        self.c1 = self.new_car(30, 30, c.NORTH, c.BLUE, True, 'Player1', 50)
-        self.c2 = self.new_car(60, 30, c.SOUTH, c.PINK, True, 'Player2', 50)
+        self.c1 = self.new_car(c.START_X_2P_1, c.START_Y_2P_1, d.NORTH, c.BLUE, True, c.PLAYER_NAME, 50)
+        self.c2 = self.new_car(c.START_X_2P_2, c.START_Y_2P_2, d.SOUTH, c.PINK, True, 'Player2', 50)
         self.counter = 0
 
     def paintEvent(self, e):
@@ -33,8 +34,7 @@ class LokMulti(board.Board):
                 if i != car:
                     self.test_collision(car, i)
         except (expections.OutOfMapError, expections.SelfDestruction, expections.Collision):
-            self.print_out(self.big_out, car.name + ' lost!')
-            self.end()
+            car.lost = True
 
     def extra_length(self):
         self.counter = self.counter + 1
@@ -42,11 +42,33 @@ class LokMulti(board.Board):
              for i in self.car_list:
                  i.add_tail(10)
 
+    def test_crash(self, car_list):
+        for i in car_list:
+            for j in car_list:
+                if i is not j and i.x == j.x and i.y == j.y:
+                    i.lost = True
+                    j.lost = True
+        alive = 0
+        for i in car_list:
+            if not i.lost:
+                alive = alive + 1
+                winner = i
+                if alive >= 2:
+                    break
+        if alive == 0:
+            self.print_out(self.big_out, 'Tie!')
+            self.end()
+        elif alive == 1:
+            self.print_out(self.big_out, winner.name + ' won!')
+            self.end()
+        else:
+            for i in car_list:
+                if i.lost:
+                    car_list.remove(i)
+
     def timerEvent(self, e):
         self.extra_length()
         self.cars_move(self.car_list)
-        if self.c1.x == self.c2.x and self.c1.y == self.c2.y:   # Die Autos fahren gegeneinander
-            self.print_out(self.big_out, 'Tie!')
-            self.end()
+        self.test_crash(self.car_list)
         self.update()
 
