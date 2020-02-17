300382f4f50f8e2e570ecf757182aca0bce4aa80
Niklas Markert <bt709885@uni-bayreuth.de>
2020-02-17 19:56:57 +0000

    Replace multiplayermode.py

--- modes/multiplayermode.py
+++ modes/multiplayermode.py
@@ -1,6 +1,5 @@
 from PyQt5.QtGui import QPainter, QColor
-import constants as c
-from constants import Directions as d
+from constants import Directions as d, StartingPositions as s, Colors as c, Options as o
 import expections
 import board
 
@@ -11,8 +10,8 @@ class LokMulti(board.Board):
 
     def start(self):
         self.car_list = []
-        self.c1 = self.new_car(c.START_X_2P_1, c.START_Y_2P_1, d.NORTH, c.BLUE, True, c.PLAYER_NAME, 50)
-        self.c2 = self.new_car(c.START_X_2P_2, c.START_Y_2P_2, d.SOUTH, c.PINK, True, 'Player2', 50)
+        self.c1 = self.new_car(s.START_X_2P_1, s.START_Y_2P_1, d.NORTH, c.BLUE, o.PLAYER_NAME, 50)
+        self.c2 = self.new_car(s.START_X_2P_2, s.START_Y_2P_2, d.SOUTH, c.PINK, 'Player2', 50)
         self.counter = 0
 
     def paintEvent(self, e):
@@ -21,6 +20,8 @@ class LokMulti(board.Board):
         painter.end()
 
     def test_collision(self, car, enemy):
+        """ Testet ob ein Auto (car) in den Schweif eines anderen Autos (enemy) fährt
+        """
         tail_arr = enemy.get_tail()
         for i in tail_arr:
             if car.x == i.x and car.y == i.y:
@@ -29,7 +30,7 @@ class LokMulti(board.Board):
 
     def move(self, car):
         try:
-            car.move(1)
+            car.move()
             for i in self.car_list:
                 if i != car:
                     self.test_collision(car, i)
@@ -37,17 +38,24 @@ class LokMulti(board.Board):
             car.lost = True
 
     def extra_length(self):
+        """ Länge des Schweifes wird für beide Autos alle 100 Ticks verlängert 
+        """
         self.counter = self.counter + 1
         if self.counter % 100 == 0:
              for i in self.car_list:
                  i.add_tail(10)
 
-    def test_crash(self, car_list):
+    def update_cars(self, car_list):
+        """ Aktualliesiert den "Lebens-Status" der Autos und checkt ob es einen Gewinner gibt
+        """
+        # Für jedes Paar von zwei Autos wird gecheckt ob sie sich gegenseitig in den "Kopf" fahren
+        # -> falls jahr wird bei beiden lost auf True gesetzt
         for i in car_list:
             for j in car_list:
                 if i is not j and i.x == j.x and i.y == j.y:
                     i.lost = True
                     j.lost = True
+        # Es werden alle Autos durchgegangen und geschaut wieviele noch leben
         alive = 0
         for i in car_list:
             if not i.lost:
@@ -55,16 +63,18 @@ class LokMulti(board.Board):
                 winner = i
                 if alive >= 2:
                     break
-        if alive == 0:
+        if alive == 0:      # Sollte keines mehr leben gibt es ein Unentschieden
             self.print_out(self.big_out, 'Tie!')
             self.end()
-        elif alive == 1:
+        elif alive == 1:    # Lebt nur noch eins, hat dieses gewonnen
             self.print_out(self.big_out, winner.name + ' won!')
             self.end()
-        else:
+        else:               # Leben noch mindestens 2, werden alle Autos die gestorben werden entfernt
             for i in car_list:
                 if i.lost:
                     car_list.remove(i)
+            # Es werden noch alle lebenden Autos durchgegangen und geschaut ob nur noch KI-Autos leben
+            # -> Falls ja, wird das Spiel beendet und die KI generell als Gewinner gesehen
             only_ai = True
             for i in car_list:
                 if not i.ai:
@@ -78,6 +88,6 @@ class LokMulti(board.Board):
     def timerEvent(self, e):
         self.extra_length()
         self.cars_move(self.car_list)
-        self.test_crash(self.car_list)
+        self.update_cars(self.car_list)
         self.update()
 
